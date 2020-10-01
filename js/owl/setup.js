29c2c6e33efa6a5fcf2f6fb64548bb2a01a40289
matheus13f <matheus-goes13@hotmail.com>
2020-10-01 12:41:23 -0300

    Netflix Clone

---
+++ js/owl/setup.js
@@ -0,0 +1,28 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 4
+        },
+        1000: {
+            items: 8
+        }
+    }
+})
+
+function assistirAgora(){
+    window.open("https://www.youtube.com/watch?v=do_BCA-vR9E");
+}
+
+function netflixAgora(){
+    window.open("https://www.netflix.com/browse");
+}
+
+function alerta(){
+    alert("Selecione um perfil para entrar!");
+}
